640d949f36ab28f5bfb0df8c425f7c85e3f7a5ad
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2022-09-07 01:41:16 +0530

    Setup the project with postgreSQL

---
+++ src/main/java/com/abishethvarman/springbootfreshproject/SpringBootFreshProjectApplication.java
@@ -0,0 +1,17 @@
+package com.abishethvarman.springbootfreshproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootFreshProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreshProjectApplication.class, args);
+	}
+
+
+
+
+}
---
+++ src/test/java/com/abishethvarman/springbootfreshproject/SpringBootFreshProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abishethvarman.springbootfreshproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFreshProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
